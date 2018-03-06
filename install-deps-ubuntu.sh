5b763f94fdba29da50157eb7dda4e709135a6c74
c4pt00 <harley0006@gmail.com>
2018-03-06 14:39:52 -0500

    Update install-deps-ubuntu.sh

--- install-deps-ubuntu.sh
+++ install-deps-ubuntu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "Sophia chatbot ubuntu 14.04 deps"
 sudo apt-get install git python-pip python-yaml python-pandas python-twisted-words
-sudo pip install flask
-sudo pip install num2words
+sudo pip install flask num2words
