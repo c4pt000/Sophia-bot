91c57659db19a2096027173c5b7247b89947afe2
c4pt00 <harley0006@gmail.com>
2018-03-06 14:54:51 -0500

    Update install-deps-centos.sh

--- install-deps-centos.sh
+++ install-deps-centos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo yum -y install epel-release
-sudo yum -y install git python-yaml python-pip python-pandas python-twisted-words
-pip install flask num2words requests
+sudo yum -y install git python-yaml python-pip python-twisted-words
+pip install flask num2words requests pandas
