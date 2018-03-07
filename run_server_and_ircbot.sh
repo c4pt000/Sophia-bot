1539b2e15e4822352a98aca258ccbdf0ba5e2b2c
c4pt00 <harley0006@gmail.com>
2018-03-06 22:33:52 -0500

    Update run_server_and_ircbot.sh

--- run_server_and_ircbot.sh
+++ run_server_and_ircbot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo nohup python ./scripts/run_server.py &
-echo "starting run_server.py loading sophia personality pausing for 30 seconds"
-sleep 30
+echo "starting run_server.py loading sophia personality pausing for 10 seconds"
+sleep 10
 echo "....."
 echo "loading Sophia irc bot"
 echo "....."
-echo "check server config in Sophia.py for channel botname and server pause for 20 seconds"
-sleep 20
+echo "check server config in Sophia.py for channel botname and server pause for 10 seconds"
+sleep 10
 echo "....."
 echo "loading..."
 sleep 2
