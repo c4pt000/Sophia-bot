807f35afb5a5a8d769b9f353d2bc44d9ad38a66d
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-06-05 00:50:42 -0400

    Update run_server_and_noirc.sh

--- run_server_and_noirc.sh
+++ run_server_and_noirc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo nohup python ./scripts/run_server.py &
-echo "starting run_server.py loading sophia personality pausing for 10 seconds"
-sleep 10
+echo "starting run_server.py loading sophia personality pausing for 2 seconds"
+sleep 2
 echo "....."
 echo "loading Sophia chat client"
 echo "....."
-sleep 10
+sleep 2
 echo "....."
 echo "loading..."
 sleep 2
