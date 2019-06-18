5dbea83bab5a2318c04fd1e0ac6ca6e9dd806aaf
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-06-18 08:13:47 -0400

    Update install-macos-deps.sh

--- install-macos-deps.sh
+++ install-macos-deps.sh
@@ -6,3 +6,7 @@ easy_install pip
 pip install --ignore-installed six
 pip install --ignore-installed numpy
 pip install flask pyyaml pandas flask num2words twisted requests
+
+sleep 1
+echo 'sh macOS_run_server_and_noirc.sh to run command line ai script will pause for 2 seconds'
+sleep 2
