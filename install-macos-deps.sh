25db2d076f03c655548f3db70600175bcd9c76e2
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-06-18 08:04:36 -0400

    changes for mojave

--- install-macos-deps.sh
+++ install-macos-deps.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-sudo easy_install pip
-sudo pip install flask pyyaml pandas flask num2words twisted requests
+echo 'run as root'
+easy_install pip
+pip install --ignore-installed six
+pip install --ignore-installed numpy
+pip install flask pyyaml pandas flask num2words twisted requests
