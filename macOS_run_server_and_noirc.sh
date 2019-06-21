095649fcb2724ed30656ed1b12561db60a470c55
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-06-20 20:49:42 -0400

    Update macOS_run_server_and_noirc.sh

--- macOS_run_server_and_noirc.sh
+++ macOS_run_server_and_noirc.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 nohup python ./scripts/run_server.py &
-echo "starting run_server.py loading sophia personality pausing for 2 seconds"
-sleep 2
+echo "starting run_server.py loading sophia personality pausing for 25 seconds"
 echo "....."
-echo "loading Sophia chat client"
+echo "loading Sophia chat client (25 second delay)"
 echo "....."
-sleep 2
+sleep 25
 echo "....."
 echo "loading..."
 sleep 2
